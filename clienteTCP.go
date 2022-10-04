4324f8503ee15fdb8a02b35860782bacb1ca15f9
thiago gentil <thiagosgentil@gmail.com>
2022-10-03 23:17:07 -0300

    first commit

---
+++ clienteTCP.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func conexaoCliente() {
+	// conectando na porta 3030 via protocolo tcp/ip na máquina local
+	conexao, erro1 := net.Dial("tcp", "127.0.0.1:8081")
+	if erro1 != nil {
+		fmt.Println(erro1)
+		os.Exit(3)
+	}
+
+	defer conexao.Close()
+
+	requisicoes := 11
+
+	for i := 0; i <= requisicoes; i++ {
+		fmt.Println("Cliente Conectado ao Servidor")
+
+		for {
+			fmt.Println("Por favor, informe o número da bebida que você deseja consultar o valor :\n" +
+				"1: Cachaça\n" +
+				"2: Cerveja\n" +
+				"3: Champanhe\n" +
+				"4: Conhaque\n" +
+				"5: Gin\n" +
+				"6: Licor\n" +
+				"7: Tequila\n" +
+				"8: Vinho\n" +
+				"9: Vodka\n" +
+				"10: Whisky\n" +
+				"0: Sair")
+			fmt.Print("Opção escolhida: ")
+			leitor := bufio.NewReader(os.Stdin)
+			texto, erro2 := leitor.ReadString('\n')
+			if erro2 != nil {
+				fmt.Println(erro2)
+				os.Exit(3)
+			}
+
+			fmt.Fprintf(conexao, texto+"\n")
+
+			// ouvindo a resposta do servidor (eco)
+			mensagem, err3 := bufio.NewReader(conexao).ReadString('\n')
+			if err3 != nil {
+				fmt.Println(err3)
+				os.Exit(3)
+			}
+			// escrevendo a resposta do servidor no terminal
+			fmt.Print("Resposta do servidor: " + mensagem)
+			time.Sleep(time.Second * 2)
+		}
+	}
+}
+
+func main() {
+
+	conexaoCliente()
+
+}
---
+++ serverTCP.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func conexaoServidor() {
+
+	fmt.Println("Servidor aguardando conexões...")
+
+	ln, erro1 := net.Listen("tcp", ":8081")
+	if erro1 != nil {
+		fmt.Println(erro1)
+
+		os.Exit(3)
+	}
+
+	// aceitando conexões
+	conexao, erro2 := ln.Accept()
+	if erro2 != nil {
+		fmt.Println(erro2)
+		os.Exit(3)
+	}
+
+	defer ln.Close()
+
+	fmt.Println("Conexão aceita...")
+
+	for {
+
+		mensagem, erro3 := bufio.NewReader(conexao).ReadString('\n')
+		if erro3 != nil {
+			fmt.Println(erro3)
+			os.Exit(3)
+		}
+
+		fmt.Print("Mensagem recebida:", string(mensagem))
+		novamensagem := teste(string(mensagem))
+
+		conexao.Write([]byte(novamensagem + "\n"))
+
+	}
+}
+
+func teste(mensagem string) string {
+	// eliminando a quebra de parágrafo e de linha
+	msg := mensagem[0:(len(mensagem) - 2)]
+	switch msg {
+	case "1":
+		fmt.Println("R$ 10,00")
+		return "R$ 10,00"
+	case "2":
+		fmt.Println("R$ 15,00")
+		return "R$ 15,00"
+	case "3":
+		fmt.Println("R$ 18,00")
+		return "R$ 18,00"
+	case "4":
+		fmt.Println("R$ 30,00")
+		return "R$ 30,00"
+	case "5":
+		fmt.Println("R$ 35,00")
+		return "R$ 35,00"
+	case "6":
+		fmt.Println("R$ 37,00")
+		return "R$ 37,00"
+	case "7":
+		fmt.Println("R$ 55,00")
+		return "R$ 55,00"
+	case "8":
+		fmt.Println("R$ 60,00")
+		return "R$ 60,00"
+	case "9":
+		fmt.Println("62,00")
+		return "R$ 62,00"
+	case "10":
+		fmt.Println("65,00")
+		return "R$ 65,00"
+	default:
+		return "Não identificado"
+
+	}
+
+}
+
+func main() {
+	conexaoServidor()
+}
